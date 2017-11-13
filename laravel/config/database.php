954793a916f4aff153a99d337a9efda898f98e18
sigurdurha320 <sigurdurhj320@nemi.tskoli.is>
2017-11-13 21:08:43 +0000

    start over

--- laravel/config/database.php
+++ laravel/config/database.php
@@ -43,7 +43,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'Laravel'),
+            'database' => env('DB_DATABASE', 'SI'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'GoodShit28+3'),
             'unix_socket' => env('DB_SOCKET', ''),
