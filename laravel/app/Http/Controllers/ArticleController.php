960e71f119ba82f23e29bc638554bdc5a5ec13a4
sindripa <sindripa@gmail.com>
2017-11-22 11:07:26 +0000

    help

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -8,6 +8,26 @@ class ArticleController extends Controller
 {
     public function index()
     {
+    	return view('home');
+    }
 
+    public function store()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+
+    public function show()
+    {
+    	
     }
 }
