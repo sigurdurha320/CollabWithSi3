fa77476244940972cff736df65be4080580dbd24
sigurdurha320 <sigurdurhj320@nemi.tskoli.is>
2017-11-24 23:05:26 +0000

    got home displaying all articles, and spesific articles

--- laravel/app/Article.php
+++ laravel/app/Article.php
@@ -6,11 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-
+	protected $table = 'article';
 	protected $fillable = ['title','privacy','image'];
     public function comments()
     {
-    	return $this->hasMany(Comment::class);	
+    	return $this->hasMany(Comment::class);
     }
     public function path()
     {
