a377f235cdd00325de7a794c02f4c03ee3c83c20
sindripa <sindripa234@nemi.tskoli.is>
2017-11-24 23:08:49 +0000

    attempted color changes

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    <div style="align-items: center; display: flex; justify-content: center; position: relative;">
+    <div style="height: 90vh; align-items: center; display: flex; justify-content: center; position: relative;">
         <div style="text-align: center;">
             <div style="font-size: 84px; margin-bottom: 30px;">
                 OurBook
             </div>
         </div>
     </div>
+@endsection
