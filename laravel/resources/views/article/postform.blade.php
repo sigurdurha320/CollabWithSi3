5ab8741476d5a4355ab37dc487e3e61ee51ee300
sindripa <sindripa234@nemi.tskoli.is>
2017-11-29 12:12:42 +0000

    attempted postform

--- laravel/resources/views/article/postform.blade.php
+++ laravel/resources/views/article/postform.blade.php
@@ -5,21 +5,31 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
+                    <div class="panel-heading"><h3>Upload Form</h3></div>
+
+                    <div class="panel-body">
+
+                    </div>
                     <form method="POST">
+                        <label class="form-group">Title</label>
+                        <input class="form-group" type="text" name="article.title"><br>
                         <div class="form-group">
                             <textarea name="contents" class="form-contol"></textarea>
                         </div>
                         <div class="form-group">
-                            <button type="submit">add comment</button>
+                            <button type="submit">Submit</button>
                         </div>
                     </form>
                     <div class="panel-body">
-                        @foreach($article->comments as $comment)
-                            <li>{{$comment->contents}}</li>
-                        @endforeach
                     </div>
                 </div>
             </div>
         </div>
     </div>
 @endsection
+
+
+
+
+/*[article.id, article.title, article.user_id, article.image_id, article.textContent, article.privacy]
+[image.id, image.img]*/
